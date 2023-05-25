5aca5c0c92baabed22c9c94521c693e1610d81f2
jvmoreirad <jvitormoreirad@hotmail.com>
2023-05-24 21:43:17 -0300

    feat: less regex more code

--- cmd/tokens/tokens.go
+++ cmd/tokens/tokens.go
@@ -2,54 +2,99 @@ package tokens
 
 import (
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-func isId(str string) bool {
-	match, _ := regexp.MatchString(`a-zA-Z`, str)
+func isValid(str string) bool {
 
-	return match
+	for _, char := range str {
+		if strings.ContainsAny(string(char), "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+			return true
+		}
+	}
+	return false
 }
+
 func isOperator(str string) bool {
-	match, _ := regexp.MatchString(`[+\-*/]`, str)
-	return match
-}
-func isNoDerivable(str string) bool {
-	match, _ := regexp.MatchString(`[\(\)\;\}\{]`, str)
-	return match
-}
-func isComparable(str string) bool {
-	match, _ := regexp.MatchString(`<=|>=|==|!=|=`, str)
-	return match
-}
-func isLogical(str string) bool {
-	match, _ := regexp.MatchString(`&|\|`, str)
-	return match
+
+	for _, char := range str {
+		if char != '+' && char != '-' && char != '*' && char != '/' {
+			return false
+		}
+
+	}
+	return len(str) > 0
 }
 
-func isInteger(str string) bool {
-	match, _ := regexp.MatchString(`\b\d+\b`, str)
-	return match
-}
-func isFloat(str string) bool {
-	match, _ := regexp.MatchString(`\b\d+\.\d+\b`, str)
-	return match
-}
-func isReserved(str string) bool {
-	match, _ := regexp.MatchString(`\b(for|while|do|return|int|float|if|else)\b`, str)
-	return match
-}
-
-func isIncrementOrDecrement(str string) bool {
-	match, _ := regexp.MatchString(`--|\+\+`, str)
-	return match
-}
 func isWhiteSpace(str string) bool {
 	for _, char := range str {
 		if char != ' ' && char != '\t' && char != '\n' && char != '\r' {
 			return false
 		}
 	}
+	return len(str) > 0
+}
+func isNoDerivable(str string) bool {
+	for _, char := range str {
+		if char != ')' && char != '(' && char != '}' && char != '{' && char != ';' {
+			return false
+		}
+	}
+	return len(str) > 0
+}
+
+func isLogical(str string) bool {
+	for _, char := range str {
+		if char != '&' && char != '|' {
+			return false
+		}
+	}
+	return len(str) > 0
+}
+
+func isComparable(str string) bool {
+
+	operators := []string{"<=", ">=", "==", "!=", "=", ">", "<"}
+
+	for _, op := range operators {
+		if str == op {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isInteger(str string) bool {
+
+	_, err := strconv.Atoi(str)
+	return err == nil
+}
+
+func isFloat(str string) bool {
+
+	sides := strings.Split(str, ".")
+	if len(sides) != 2 {
+		return false
+	}
+	rightSide := sides[0]
+	leftSide := sides[1]
+	_, err := strconv.Atoi(rightSide)
+	if err != nil {
+		return false
+	}
+	_, err = strconv.Atoi(leftSide)
+	if err != nil {
+		return false
+	}
 	return true
+
+}
+
+func isReserved(str string) bool {
+	match, _ := regexp.MatchString(`\b(for|while|do|return|int|float|if|else)\b`, str)
+	return match
 }
 
 func TokenMaker(str string) []string {
@@ -57,10 +102,57 @@ func TokenMaker(str string) []string {
 	var stack string
 	for _, char := range str {
 		switch {
+		case isReserved(string(char)):
+			if stack != "" {
+				if !isReserved(stack) {
+					tokens = append(tokens, stack)
+					stack = string(char)
+				} else {
+					stack += string(char)
+				}
+			} else {
+				stack = string(char)
+			}
+		case isLogical(string(char)):
+			if stack != "" {
+				if !isReserved(stack) {
+					tokens = append(tokens, stack)
+					stack = string(char)
+				} else {
+					stack += string(char)
+				}
+			} else {
+				stack = string(char)
+			}
 		case isWhiteSpace(string(char)):
 			if stack != "" {
 				tokens = append(tokens, stack)
 				stack = ""
+			}
+		case string(char) == ".":
+			if stack != "" {
+				if isInteger(stack) {
+					stack += string(char)
+				} else {
+					tokens = append(tokens, stack)
+					stack = ""
+				}
+			} else {
+				stack += string(char)
+			}
+
+		case isInteger(string(char)):
+			if stack != "" {
+				if isFloat(stack + string(char)) {
+					stack += string(char)
+				} else if !isInteger(stack) {
+					tokens = append(tokens, stack)
+					stack = string(char)
+				} else {
+					stack += string(char)
+				}
+			} else {
+				stack = string(char)
 			}
 
 		case isNoDerivable(string(char)):
@@ -69,6 +161,7 @@ func TokenMaker(str string) []string {
 				stack = ""
 			}
 			tokens = append(tokens, string(char))
+
 		case isOperator(string(char)):
 			if stack == string(char) {
 				tokens = append(tokens, stack+string(char))
@@ -79,12 +172,35 @@ func TokenMaker(str string) []string {
 				}
 				stack = string(char)
 			}
+		case isComparable(string(char)):
+			if stack != "" {
+				if !isComparable(stack) {
+					tokens = append(tokens, stack)
+					stack = string(char)
+				} else {
+					stack += string(char)
+				}
+			} else {
+				stack = string(char)
+			}
 
+		case isValid(string(char)):
+			if stack != "" {
+				if !isValid(stack) {
+					tokens = append(tokens, stack)
+					stack = string(char)
+				} else {
+					stack += string(char)
+				}
+			} else {
+				stack = string(char)
+			}
 		default:
 			stack += string(char)
 		}
 
 	}
+	tokens = append(tokens, stack)
 	return tokens
 }
 
@@ -92,7 +208,7 @@ func TokenFormatter(tokens []string) []string {
 	var word []string
 	for _, tok := range tokens {
 		if tok != "" {
-			if isComparable(tok) || isLogical(tok) || isNoDerivable(tok) || isOperator(tok) || isReserved(tok) || isIncrementOrDecrement(tok) {
+			if isComparable(tok) || isLogical(tok) || isNoDerivable(tok) || isOperator(tok) || isReserved(tok) {
 				word = append(word, "<"+tok+">")
 			} else if isFloat(tok) {
 				word = append(word, "<"+tok+", float>")
@@ -106,5 +222,3 @@ func TokenFormatter(tokens []string) []string {
 
 	return word
 }
-
-// improvement
